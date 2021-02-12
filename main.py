89e02000db031d918a42060cfd37ec7d73513499
Abhijeet Satpute <abhi111sat@gmail.com>
2021-02-12 13:52:52 +0530

    write to csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import lxml
+import csv
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 import twilio_info
@@ -7,8 +8,14 @@ import twilio_info
 if __name__ == "__main__":
     url = input("Enter the product Link: ")
     mobile_num = input("\nEnter your Phone number to recieve Alerts: +91-")
+    price_set = input("\nEnter the Target Price: ")
 
-    HEADERS = {'User-Agent': 'Mozilla/5.0'}
+    with open('track.csv', mode='w') as tracker:
+        tracker_writer = csv.writer(tracker, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        tracker_writer.writerow(['Product', 'Number', 'TargetPrice'])
+        tracker_writer.writerow([url, mobile_num, price_set])
+
+    HEADERS = { "user-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
     req = requests.get(url,headers=HEADERS)
     soup = BeautifulSoup(req.content, 'lxml')
     price = (soup.find(id='priceblock_ourprice').text)
