b52182045a20cc2d0d841179f6b41667802d7d2c
Abhijeet Satpute <abhi111sat@gmail.com>
2021-02-12 14:37:31 +0530

    Create track.py

---
+++ track.py
@@ -0,0 +1,30 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+from twilio.rest import Client
+import write_csv
+import pandas
+
+#Product info
+csv_data = pandas.read_csv('track.csv')
+url = str(csv_data.loc[0,'Product'])
+mobile_num = str(csv_data.loc[0,'Number'])
+price_set = str(csv_data.loc[0,'TargetPrice'])
+
+HEADERS = { "user-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+req = requests.get(url,headers=HEADERS)
+soup = BeautifulSoup(req.content, 'lxml')
+price = (soup.find(id='priceblock_ourprice').text)
+product = soup.find(id='productTitle').text.strip()
+price_curr = float((price.split()[1]).replace(',',''))
+
+if(price_curr < price_set):
+    twilio_info = pandas.read_csv('twilio_info.csv')
+    account_sid = str(twilio_info.loc[0,'account_sid'])
+    auth_token = str(twilio_info.loc[0,'auth_token'])
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+                              from_='whatsapp:+14155238886',
+                              body='Hello! This is a WhatsAppPriceAlert. \n\nYour Price Drop Alert for *'+product+'\n* \nCurrent Price: *'+price+'*'+'\nBuy it ASAP !!\n\n'+url,
+                              to='whatsapp:+91' + mobile_num
+                          )
